883a67a1fde97f112d0d318ea6772b7c710518cc
stevenxu27 <steven.x2085@gmail.com>
2024-09-24 23:26:12 -0700

    Doxygen commenting

--- launch/image_converter.py
+++ launch/image_converter.py
@@ -14,8 +14,22 @@ roslib.load_manifest('enph353_ros_lab')
 
 
 class image_converter:
+    """
+    A class that converts ROS image messages to OpenCV format and
+        processes them.
+
+    This class subscribes to a ROS topic for raw image data, processes the
+        image to detect
+    circles, and publishes robot movement commands based on the detected
+        circle's position.
+    """
 
     def __init__(self):
+        """
+        Initializes the image_converter class.
+
+        Sets up image publisher and subscriber, and initializes the CvBridge.
+        """
         self.image_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/rrbot/camera1/image_raw", Image,
@@ -23,14 +37,25 @@ class image_converter:
         self.constant = 0
 
     def callback(self, data):
+        """
+        Callback function to process incoming image data.
+
+        Converts the ROS Image message to a CV2 image and
+            processes it to find circles.
+        Calculates movement commands based on the circle's
+            position relative to the image center.
+
+        :param data: The incoming ROS Image message.
+        """
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
         height, width = cv_image.shape[:2]
 
+        # creates movement object to publish to
         move = Twist()
-        rate = rospy.Rate(30)  # 10 Hz
+        rate = rospy.Rate(30)  # set rate to 30Hz
 
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
@@ -39,6 +64,7 @@ class image_converter:
         crossed_in = False
         crossed_out = True
 
+        # iterates through the gray scale images
         for index, value in enumerate(gray[-1]):
             if value < 150 and not crossed_in:
                 circle_left = index
@@ -48,6 +74,7 @@ class image_converter:
                 circle_right = index
                 crossed_out = True
 
+        # find center and comparison values
         center = int((circle_right - circle_left) / 2) + circle_left
         comp_val = int(width / 2)
 
@@ -65,15 +92,11 @@ class image_converter:
                 move.angular.z = -5.5
             else:
                 move.angular.z = -4.5 * np.abs(center - comp_val) / comp_val
-
+        # Edge case for bend when path extends across screen
         if (circle_left > comp_val and circle_right < comp_val):
             move.angular.z = -7.0
-        # if (circle_right < comp_val and circle_right < 200):
-        #     move.angular.z = 4.0
-        # if (circle_left > comp_val and circle_right > 500):
-        #     move.angular.z = -6.0
 
-        # Draw circle
+        # Draw circle for testing
         center_pt = (int((circle_right - circle_left)/2) + circle_left,
                      height - 50)
         radius = 20
@@ -87,18 +110,7 @@ class image_converter:
         self.image_pub.publish(move)
         rate.sleep()
 
-        # for index, value in enumerate(gray[-1]):
-        #     if value < 150 and not crossed_in:
-        #         circle_left = index
-        #         crossed_in = True
-        #         crossed_out = False
-        #     if value > 150 and not crossed_out:
-        #         circle_right = index
-        #         crossed_out = True
-
-        # center = int((circle_right - circle_left) / 2) + circle_left
-        # comp_val = int(width / 2)
-
+        # ensure speed doesn't increase for accuracy
         move.linear.x = 0.12
         if (center < comp_val):
             move.angular.z = 0.03
@@ -106,13 +118,19 @@ class image_converter:
             move.angular.z = -0.06
         if (circle_left > comp_val and circle_right < comp_val):
             move.angular.z = -7.0
-        # if (circle_left > comp_val and circle_right > 600):
-        #     move.angular.z = -6.0
         self.image_pub.publish(move)
         rate.sleep()
 
 
 def main(args):
+    """
+    Main function to start the image converter node.
+
+    Initializes the ROS node and starts the main
+        loop to process incoming images.
+
+    :param args: Command line arguments.
+    """
     ic = image_converter()
     rospy.init_node('image_converter', anonymous=True)
     try:
